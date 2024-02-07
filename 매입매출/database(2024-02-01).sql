325443a47bee300a27d87cb6b88563f90f291a27
sh1nheewon <tlsgmldnjs00@naver.com>
2024-02-07 15:52:28 +0900

    조건검색

--- 매입매출/database(2024-02-01).sql
+++ 매입매출/database(2024-02-01).sql
@@ -40,9 +40,48 @@ ALTER TABLE ... 명령를 사용하여 변경한다.
     하지만 간혹 대량의 데이터가 보관되어 있는 경우, 실행이 진행되다가 
     중간에 중단되는 경우도 있다.
     이럴때 기존의 데이터에 손상이 가해지는 경우가 있다.
+    
 2. 칼럼의 이름 변경 : 연관된 여러 애플리케이션에서 해당 칼럼을 찾지 못하는 문제가 발생한다.
-	
+
+3. 제약조건 : FK, PK 등의 제약조건의 변경
+	데이터의 무결성 보장이 해제되는 문제를 일으킨다.
 */
+
+-- 칼럼의 이름과 type 변경
+-- 1. m_pass VARCHAR(50) 이라는 칼럼 생성
+-- 2. m_password 칼럼에 저장된 데이터를 m_pass 로 이동 또는 복사
+-- 3. m_password 칼럼을 삭제
+ALTER TABLE tbl_members
+CHANGE m_password m_pass VARCHAR(50);
+DESC tbl_members;
+
+ALTER TABLE tbl_members
+CHANGE m_pass m_password VARCHAR(125);
+
+-- 칼럼의 type 만 변경
+ALTER TABLE tbl_members
+MODIFY m_password VARCHAR(255);
+
+-- 칼럼의 이름만 변경
+ALTER TABLE tbl_members
+CHANGE m_password m_pass VARCHAR(50);
+
+-- FK 생성
+ALTER TABLE tbl_iolist
+ADD CONSTRAINT FK_PCODE
+FOREIGN KEY (io_pcode)
+REFERENCES tbl_products(p_code);
+
+-- FK 삭제
+ALTER TABLE tbl_iolist
+DROP CONSTRAINT FK_PCODE;
+
+DESC tbl_members;
+
+SELECT * FROM tbl_members;
+
+USE ecountdb;
+SELECT * FROM tbl_iolist;
 
 
 
